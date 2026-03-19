fa1cd64adada2e4a0e19ee4544726b27cc1df7aa
SKPearlman31 <140081207+SKPearlman31@users.noreply.github.com>
2026-03-19 11:14:00 -0400

    feat: add footer.php

---
+++ verus-theme/footer.php
@@ -0,0 +1,10 @@
+<!-- FOOTER -->
+<footer>
+  <img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/logo.png" alt="Verus" class="footer-logo">
+  <p class="footer-text">&copy; <?php echo date('Y'); ?> Verus Basketball. All Rights Reserved.</p>
+</footer>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
